5fbc963376b709de8deda0be6f562a3591d5c0b9
RHN007 <rakibul.nayon@outlook.com>
2022-10-20 08:06:09 +0600

    adding login & singup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6iXETp68bgWLTdLMcszwNN86G0ogsbyk",
+  authDomain: "ema-john-simple-3ad9c.firebaseapp.com",
+  projectId: "ema-john-simple-3ad9c",
+  storageBucket: "ema-john-simple-3ad9c.appspot.com",
+  messagingSenderId: "203004590276",
+  appId: "1:203004590276:web:4bfb02272cfcd4a48d3191"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app; 
